cb96bd1b120a1d2887527b534e5426c0e944a175
Javier Constain <constainopia@gmail.com>
2018-05-19 17:35:08 -0500

    Adjustments to work on Windows and formatting adjustemts

--- src/main.py
+++ src/main.py
@@ -41,17 +41,26 @@ def upload_ftp ():
 
     # Scan local direcotry
     logger.info('scanning')
-    srcdir_length = len(SOURCEDIR) 
+    srcdir_length = len(SOURCEDIR)  # To extract the root path from the workir
+    # Upload process.
     for root, dirs, files in os.walk(SOURCEDIR, topdown=True):
+        # Getting work directory
         logger.info('ROOT: {}'.format(root))
         cwd = root[srcdir_length:]
+        cwd = str.replace(cwd, '\\', '/')   # Windows directory structure.
         workdir ='/{}{}'.format(WORKDIR, cwd)
+        # Go the new wordir to start uploading.
         ftp.cwd(workdir)
         logger.debug('remote pwd: {}'.format(ftp.pwd()))
 
         for file_name in files:
+            # Avoid Unix hidden files.
+            if file_name[0] == '.':
+                continue
+
             file_path = os.path.join(root, file_name)
             logger.info('FILE: {}, PATH: {}'.format(file_name, file_path))
+            # Store file.
             store_cmd = 'STOR ./{}'.format(file_name)
             with open(file_path, 'rb') as f:
                 print(store_cmd)
@@ -60,11 +69,12 @@ def upload_ftp ():
         for dir_name in dirs:
             file_path = os.path.join(root, dir_name)
             logger.info('DIR: {}, PATH: {}'.format(dir_name, file_path))
+            # Create folder if does not exists.
             files = ftp.nlst()
-	    if not dir_name in files:
+            if not (dir_name in files):
                 ftp.mkd(dir_name)
 
-    logger.info('Finished updload.')
+    logger.info('Finished upload.')
     ftp.quit()
 
 
